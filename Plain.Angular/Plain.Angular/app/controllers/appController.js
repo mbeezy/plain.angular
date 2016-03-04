ed682e8b7bde0839caf3c4b7df7c0eb6a7beb100
mbeezy <bolanos.mike@mail.com>
2016-03-04 12:00:24 -0800

    Inital Commit

---
+++ Plain.Angular/Plain.Angular/app/controllers/appController.js
@@ -0,0 +1,10 @@
+﻿window.app.controller('appController', appController);
+
+appController.$inject = [];
+
+function appController() {
+
+    var vm = this;
+    vm.messageInput = 'Hello There';
+
+}
